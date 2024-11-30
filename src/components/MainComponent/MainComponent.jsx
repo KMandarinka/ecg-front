ca949445f1d1a9f07581e55d91dfe7f1bb4e6085
KMandarinka <katerinaa.gv03@gmail.com>
2024-11-30 15:45:09 +0300

    Changes in MainComponent, new button + text

--- src/components/MainComponent/MainComponent.jsx
+++ src/components/MainComponent/MainComponent.jsx
@@ -4,23 +4,38 @@ import "../MainComponent/MainComponent.css"; // Импорт стилей
 const MainComponent = () => {
   return (
     <div className="main-container">
-      {/* Top section of the main container (below the header) */}
-      <div className="top-container">
-        <h1 className="hearton-text">HeartON</h1>
-        <p className="description-text">Приложение для анализа сигналов ЭКГ, использующего алгоритмы машинного обучения.</p>
+      {/* Header section with links */}
+      <div className="header-container">
+        <span className="breadcrumb">Главная > </span>
+        <span className="contact-link">Остались вопросы? Напишите нам</span>
       </div>
 
-      {/* Bottom container with buttons */}
+      {/* Top section of the main container */}
+      <div className="top-container">
+        <h1 className="hearton-text">HeartON</h1>
+        {/* Добавляем полосочки и текст */}
+        <div className="line-container">
+          <div className="line"></div>
+          <span className="since-text">since 2024</span>
+          <div className="line"></div>
+        </div>
+        <p className="description-text">Приложение для анализа сигналов ЭКГ, использующее алгоритмы машинного обучения.</p>
+      </div>
+
       <div className="bottom-container">
+      <div className="top-buttons-container">
         <button className="button">
           <div className="first-line">Загрузите файлы ЭКГ</div>
           <div className="second-line">или перетащите файлы</div>
         </button>
         <button className="button">
-          <div className="first-line">История анализов</div>
-          <div className="second-line">ЭКГ</div>
+          <div className="first-line">История анализов ЭКГ</div>
         </button>
       </div>
+      <button className="how-it-works-button">Как работает программа?</button>
+    </div>
+
+
     </div>
   );
 };
